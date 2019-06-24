77af67508e5567bc3a938e209d1faa457ddb1114
Jay Maas <jaymaas.dev@gmail.com>
2019-06-24 16:03:38 -0600

    Deployment Config & Init. - 1
    Modified the appropriate files to enable deployment to Heroku via Postgress. The pg dependency has also been added.

--- knexfile.js
+++ knexfile.js
@@ -1,4 +1,16 @@
+const dbConnection = process.env.DATABASE_URL
+
 module.exports = {
+    production: {
+      client: 'pg',
+      connection: dbConnection,
+      migrations: {
+        directory: './data/migrations',
+      },
+      seeds: {
+        directory: './data/seeds',
+      },
+    },
     development: {
       client: 'sqlite3',
       connection: {
